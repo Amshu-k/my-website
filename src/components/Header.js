a2540027ac699b4e926e94eb894d159b64892dbc
Amshuman Krishnamurthy <akrishnamurthy@catalogicsoftware.com>
2020-09-25 17:19:30 -0400

    Added particles

---
+++ src/components/Header.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import avatar from '../avatar.png';
+import Typed from "react-typed";
+import {
+    Typography,
+    Avatar,
+    Grid,
+    Box
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+
+const useStyles = makeStyles(theme => ({
+    title: {
+        color: 'white'
+    },
+    subtitle: {
+        color: 'tan',
+        marginBottom: "1rem"
+    },
+    typedContainer: {
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        width: "100vw",
+        textAlign: 'center',
+        zIndex: 1
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        width: theme.spacing(15),
+        height: theme.spacing(15)
+    },
+    listItem: {
+        color: 'tan'
+    }
+}))
+
+const Header = () => {
+    const classes = useStyles()
+    return (
+        <Box className={classes.typedContainer}>
+            <Grid container justify='center'>
+                <Avatar className={classes.avatar} src={avatar} alt="Amshu" />
+            </Grid>
+            <Typography className={classes.title} variant="h6">
+                <Typed
+                    strings={['Amshuman Krishnamurthy']}
+                    typeSpeed={40}
+                />
+            </Typography>
+            <br/>
+            <Typography variant="h5" className={classes.subtitle}>
+                <Typed
+                    strings={['Frontend Engineering', 'Angular', 'React', 'Node.js']}
+                    typeSpeed={40}
+                    backSpeed={15}
+                    loop
+                />
+            </Typography>
+        </Box>
+    )
+}
+
+export default Header
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,52 @@
 import React from 'react'
+import Header from './Header'
 import Navbar from './Navbar'
+import Particles from 'react-particles-js'
+import { makeStyles } from "@material-ui/core/styles";
+
+
+const useStyles = makeStyles(theme => ({
+    particles: {
+        position: 'absolute'
+    }
+}))
 
 function Home() {
+    const classes = useStyles();
     return (
-        <div>
+        <React.Fragment>
             <Navbar />
-        </div>
+            <Header />
+            <Particles
+                canvasClassName={classes.particles}
+                params={{
+                    particles: {
+                        number: {
+                            value: 50,
+                            density: {
+                                enable: true,
+                                value_area: 2000
+                            }
+                        },
+                        size: {
+                            value: 3,
+                            random: true,
+                            anim: {
+                                enable: true,
+                            }
+                        }
+                    },
+                    interactivity: {
+                        events: {
+                            onhover: {
+                                enable: true,
+                                mode: "repulse"
+                            }
+                        }
+                    }
+                }}
+            />
+        </React.Fragment >
     )
 }
 
