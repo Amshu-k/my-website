7ee19015cda346945b7755591b1e2c27a8b49559
Amshuman Krishnamurthy <akrishnamurthy@catalogicsoftware.com>
2020-10-04 22:21:47 -0400

    update css

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core/styles';
+import './Projects.css';
 import {
     Box,
     Grid,
@@ -15,116 +16,118 @@ import Navbar from './Navbar';
 
 const useStyles = makeStyles({
     mainContainer: {
-        background: '#233',
         height: '100vh'
     },
     cardContainer: {
         maxWidth: 345,
-        margin: '5rem auto'
+        margin: '1.5rem auto'
     }
 })
 
 const Projects = () => {
     const classes = useStyles();
     return (
-        <Box component='div' className={classes.mainContainer}>
-            <Navbar />
-            <Grid container justify='center'>
-                <Grid item xs={12} sm={8} md={6}>
-                    <Card className={classes.cardContainer}>
-                        <CardActionArea>
-                            <CardContent>
-                                <Typography gutterBottom variant='h5'>
-                                    POLARIS
+        <div className="overflow">
+            <Box component='div' className={classes.mainContainer}>
+                <Navbar />
+                <Grid container justify='center'>
+                    <Grid item xs={12} sm={8} md={6}>
+                        <Card className={classes.cardContainer}>
+                            <CardActionArea>
+                                <CardContent>
+                                    <Typography gutterBottom variant='h5'>
+                                        POLARIS
                                 </Typography>
-                                <Typography variant='body2' color='textSecondary' component='p'>
-                                    Chat application built using Angular and Firebase. <br /> Firebase used for authentication and storage of data.
+                                    <Typography variant='body2' color='textSecondary' component='p'>
+                                        Chat application built using Angular and Firebase. <br /> Firebase used for authentication and storage of data.
                                 </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                        <CardActions>
-                            <Button size='small' color='primary'>
-                                <a
-                                    target="_blank"
-                                    href="https://github.com/Amshu-k/angular-firebase-chat">
-                                    GitHub
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Button size='small' color='primary'>
+                                    <a
+                                        target="_blank"
+                                        href="https://github.com/Amshu-k/angular-firebase-chat">
+                                        GitHub
                                     </a>
-                            </Button>
-                            <Button size='small' color='primary'>
-                                <a
-                                    target="_blank"
-                                    href="https://chat-app-8e53f.web.app/login">
-                                    View Project
+                                </Button>
+                                <Button size='small' color='primary'>
+                                    <a
+                                        target="_blank"
+                                        href="https://chat-app-8e53f.web.app/login">
+                                        View Project
                                     </a>
-                            </Button>
-                        </CardActions>
+                                </Button>
+                            </CardActions>
 
-                    </Card>
-                </Grid>
-                <Grid item xs={12} sm={8} md={6}>
-                    <Card className={classes.cardContainer}>
-                        <CardActionArea>
-                            <CardContent>
-                                <Typography gutterBottom variant='h5'>
-                                    Newsvoice
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={8} md={6}>
+                        <Card className={classes.cardContainer}>
+                            <CardActionArea>
+                                <CardContent>
+                                    <Typography gutterBottom variant='h5'>
+                                        Newsvoice
                                 </Typography>
-                                <Typography variant='body2' color='textSecondary' component='p'>
-                                    Voice controlled React.js news application using Alan AI for voice commands and Material-UI for design.
+                                    <Typography variant='body2' color='textSecondary' component='p'>
+                                        Voice controlled React.js news application using Alan AI for voice commands and Material-UI for design.
                                 </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                        <CardActions>
-                            <Button size='small' color='primary'>
-                                <a
-                                    target="_blank"
-                                    href="https://github.com/Amshu-k/newsvoice">
-                                    GitHub
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Button size='small' color='primary'>
+                                    <a
+                                        target="_blank"
+                                        href="https://github.com/Amshu-k/newsvoice">
+                                        GitHub
                                     </a>
-                            </Button>
-                            <Button size='small' color='primary'>
-                                <a
-                                    target="_blank"
-                                    href="https://newsvoicee.web.app/">
-                                    View Project
+                                </Button>
+                                <Button size='small' color='primary'>
+                                    <a
+                                        target="_blank"
+                                        href="https://newsvoicee.web.app/">
+                                        View Project
                                     </a>
-                            </Button>
-                        </CardActions>
+                                </Button>
+                            </CardActions>
 
-                    </Card>
-                </Grid>
-                <Grid item xs={12} sm={8} md={6}>
-                    <Card className={classes.cardContainer}>
-                        <CardActionArea>
-                            <CardContent>
-                                <Typography gutterBottom variant='h5'>
-                                    Covid-19 Tracker
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={8} md={6}>
+                        <Card className={classes.cardContainer}>
+                            <CardActionArea>
+                                <CardContent>
+                                    <Typography gutterBottom variant='h5'>
+                                        Covid-19 Tracker
                                 </Typography>
-                                <Typography variant='body2' color='textSecondary' component='p'>
-                                    Angular application to display latest Covid19 statistics country wise. <br /> Developed with Angular 9 and Angular Material. Deployed the application on Docker
+                                    <Typography variant='body2' color='textSecondary' component='p'>
+                                        Angular application to display latest Covid19 statistics country wise. <br /> Developed with Angular 9 and Angular Material. Deployed the application on Docker
                                 </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                        <CardActions>
-                            <Button size='small' color='primary'>
-                                <a
-                                    target="_blank"
-                                    href="https://github.com/Amshu-k/covid19">
-                                    GitHub
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Button size='small' color='primary'>
+                                    <a
+                                        target="_blank"
+                                        href="https://github.com/Amshu-k/covid19">
+                                        GitHub
                                     </a>
-                            </Button>
-                            <Button size='small' color='primary'>
-                                <a
-                                    target="_blank"
-                                    href="https://hub.docker.com/r/gandalf7/covid19">
-                                    Docker
+                                </Button>
+                                <Button size='small' color='primary'>
+                                    <a
+                                        target="_blank"
+                                        href="https://hub.docker.com/r/gandalf7/covid19">
+                                        Docker
                                     </a>
-                            </Button>
-                        </CardActions>
+                                </Button>
+                            </CardActions>
 
-                    </Card>
+                        </Card>
+                    </Grid>
                 </Grid>
-            </Grid>
-        </Box>
+            </Box>
+        </div>
+
     )
 }
 
